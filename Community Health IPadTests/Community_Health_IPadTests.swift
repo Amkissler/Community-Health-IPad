649ee2c4077d91ea84322d90af1f2cc78e82b427
Aaron Kissler <aaronkissler@aarons-macbook-pro.local>
2016-01-10 10:46:53 -0500

    Initial Commit

---
+++ Community Health IPadTests/Community_Health_IPadTests.swift
@@ -0,0 +1,36 @@
+//
+//  Community_Health_IPadTests.swift
+//  Community Health IPadTests
+//
+//  Created by Aaron Kissler on 1/10/16.
+//  Copyright © 2016 Aaron Kissler. All rights reserved.
+//
+
+import XCTest
+@testable import Community_Health_IPad
+
+class Community_Health_IPadTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
